5347a0bbdf4636680cb0fe9dbf4febb9d79f8019
Krishna Sahoo <krishnasahoo0368@gmail.com>
2026-05-26 16:16:59 +0530

    Add files via upload

---
+++ HotelBill.java
@@ -0,0 +1,17 @@
+package HelloWorld;
+
+public class HotelBill {
+
+	public static void main(String[] args) {
+		System.out.println("******HOTEL Bill*******");
+		System.out.println("Restaurant Name: ABC Hotel");
+		System.out.println("Food Items: ");
+		System.out.println("1.Fried Rice : 120");
+		System.out.println("2.Biriyani : 130");
+		System.out.println("3. Ice Cream : 100");
+		System.out.println("******Total Bill******");
+		System.out.println("350");
+		
+	}
+
+}
