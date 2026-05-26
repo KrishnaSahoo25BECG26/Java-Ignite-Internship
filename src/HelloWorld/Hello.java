98df9e1a06bb78b2a19688307b283787083bc99f
Krishna Sahoo <krishnasahoo0368@gmail.com>
2026-05-26 17:00:22 +0530

    Day 1 Java Assignment

---
+++ src/HelloWorld/Hello.java
@@ -0,0 +1,9 @@
+package HelloWorld;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("******HOTEL Bill********\nRestaurant Name : ABC Hotel\nFoodItems:\n1.Fried Rice: 120\n2.Biriyani: 130\n3.Pizza: 110\nTotal Amount: 360\n************************");
+	}
+
+}
---
+++ src/HelloWorld/timeTable.java
@@ -0,0 +1,16 @@
+package HelloWorld;
+
+public class timeTable {
+
+	public static void main(String[] args) {
+		System.out.println("7:00 - Wake Up");
+		System.out.println("7:30 - Breakfast");
+		System.out.println("8:00 - Internship theory session");
+		System.out.println("10:00 - Tea Break");
+		System.out.println("11:00 - Review");
+		System.out.println("13:00 - Lunch Break");
+		System.out.println("13:30 - Small Nap");
+		System.out.println("15:00 - Lab Session");
+	}
+
+}
