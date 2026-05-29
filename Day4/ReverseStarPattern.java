16ad2a9d313747382dc89a344f7a91a0fb161ded
Krishna Sahoo <krishnasahoo0368@gmail.com>
2026-05-29 16:25:59 +0530

    Add files via upload

---
+++ Day4/ReverseStarPattern.java
@@ -0,0 +1,13 @@
+
+public class ReverseStarPattern {
+
+	public static void main(String[] args) {
+		for (int i = 5; i > 0; i--) {
+			for (int j=i; j > 0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
